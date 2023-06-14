6399113a6e625864b5f7970c69d0f11c3d194bc6
Van Hien <hiench2001@gmail.com>
2023-06-14 10:52:21 -0600

    feat: rename file

---
+++ app.py
@@ -0,0 +1,12 @@
+import os
+import re
+from dotenv import load_dotenv
+load_dotenv()
+
+os.chdir(os.getenv("DOWNLOAD_PATH"))
+print(os.getcwd())
+
+for file in os.listdir():
+    name, ext = os.path.splitext(file)
+    new_name = re.sub(r'\W+', '-', name) + ext
+    os.rename(file, new_name)
